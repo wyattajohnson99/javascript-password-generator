88ab8ae31c8c97580e859baf92907c7722d1223b
Wyatt Johnson <wyattajohnson99@Wyatts-MacBook-Air.local>
2024-01-17 09:50:01 -0600

    Created arrays for the possible characters of the password

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,10 @@
-// Assignment code here
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// Arrays for the various characters that can be used for the password generator
+var specialCharacters = ['@', '%', '+', '\', '/'', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '{', '}', '[', ']', '~', '-', '_', '.'];
+var lowerCaseCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCaseCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numberCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 // Write password to the #password input
 function writePassword() {
